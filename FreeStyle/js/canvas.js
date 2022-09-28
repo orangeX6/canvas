9a3484c35e0b5087ba91315493292cc7c76a3f81
Pranav <naringrekarpranav6@live.com>
2022-09-28 20:10:11 +0530

    Fractals

--- FreeStyle/js/canvas.js
+++ FreeStyle/js/canvas.js
@@ -1,23 +1,26 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = innerWidth * 0.8;
-canvas.height = innerHeight * 0.8;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-let size = 400;
+// variables
+let branches = 2;
+let maxLevel = 4;
 let sides = 5;
-let maxLevel = 25;
-
-ctx.fillStyle = 'hotpink';
-ctx.strokeStyle = 'yellow';
+let size = 200;
+let spread = 0.5;
+let scale = 0.5;
+let color = `hsl(51, 100%, 50%)`;
+let shadowColor = `hsla(51, 100%, 50%, 0.7)`;
+let backgroundColor = `hsl(0, 0%, 0%)`;
 ctx.lineWidth = 15;
-ctx.lineCap = 'round';
+ctx.shadowBlur = 10;
+ctx.shadowOffsetX = 10;
+ctx.shadowOffsetY = 5;
 
-ctx.save();
-ctx.translate(canvas.width / 2, canvas.height / 2);
-ctx.scale(1, 1);
-ctx.rotate(0);
-// ctx.fillRect(0, 0, canvas.width, canvas.height);
-// ctx.restore();
+// canvas values
+ctx.lineCap = 'round';
+ctx.fillStyle = backgroundColor;
 
 const drawBranch = (level) => {
   if (level > maxLevel) return;
@@ -26,16 +29,40 @@ const drawBranch = (level) => {
   ctx.lineTo(size, 0);
   ctx.stroke();
 
-  ctx.translate(50, 0);
-  ctx.rotate(0.5);
-  ctx.scale(0.9, 0.9);
+  for (let i = 0; i < branches; i++) {
+    ctx.save();
+    ctx.translate(size - (size / branches) * i, 0);
+    ctx.rotate(spread);
+    ctx.scale(scale, scale);
+    drawBranch(level + 1);
+    ctx.restore();
 
-  drawBranch(level + 1);
+    ctx.save();
+    ctx.translate(size - (size / branches) * i, 0);
+    ctx.rotate(-spread);
+    ctx.scale(scale, scale);
+    drawBranch(level + 1);
+    ctx.restore();
+  }
 };
 
-drawBranch(0);
+const drawFractal = () => {
+  ctx.save();
+  ctx.strokeStyle = color;
+  ctx.shadowColor = shadowColor;
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+
+  for (var i = 0; i < sides; i++) {
+    ctx.rotate((Math.PI * 2) / sides);
+    drawBranch(0);
+  }
+  ctx.restore();
+};
+drawFractal();
 
 addEventListener('resize', () => {
-  canvas.width = innerWidth * 0.8;
-  canvas.height = innerHeight * 0.8;
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 });
